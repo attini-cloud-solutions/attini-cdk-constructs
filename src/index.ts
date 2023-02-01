a05d5bd9431cea7767273d61a32fb2a7c03583e9
oscarostrand <oscar.ostrand@gmail.com>
2023-02-01 10:17:31 +0100

    added deployment plan construct

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,61 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { CfnResource } from 'aws-cdk-lib';
+import { Chain, CustomState, State, StateGraph } from 'aws-cdk-lib/aws-stepfunctions';
+import { Construct } from 'constructs';
+
+
+export class AttiniMerge extends CustomState {
+
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id, { stateJson: [] });
   }
+
+  toStateJson(): object {
+    return {
+      Type: 'AttiniMerge',
+      ...this.renderNextEnd(),
+    };
+  }
+
+}
+
+export interface DeploymentPlanProps {
+  readonly states?: Array<State>;
+  readonly definition?: Chain;
+}
+
+export class DeploymentPlan extends Construct {
+
+
+  constructor(scope: Construct, id: string, props: DeploymentPlanProps) {
+    super(scope, id);
+
+    let states: any;
+
+    if (props.states != null) {
+      states = this.createSimplePlan(props.states);
+    } else if (props.definition) {
+      states =new StateGraph(props.definition.startState, 'not important').toGraphJson();
+    }
+
+
+    new CfnResource(this, 'DeploymentPlan', {
+      type: 'Attini::Deploy::DeploymentPlan',
+      properties: {
+        DeploymentPlan: states,
+      },
+    });
+  }
+
+  private createSimplePlan( states:Array<State>) {
+    return states.map(value => {
+      let json = <any>value.toStateJson();
+      delete json.End;
+      delete json.Next;
+      json.Name = value.id;
+      return json;
+    });
+
+  }
+
 }
---
+++ test/deployment-plan.test.ts
@@ -0,0 +1,36 @@
+import { App, Duration, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { Wait, WaitTime } from 'aws-cdk-lib/aws-stepfunctions';
+import { AttiniMerge, DeploymentPlan } from '../src';
+
+
+test('should create template from definition', () => {
+  const mockApp = new App();
+  const stack = new Stack(mockApp);
+  let props = {
+    definition:
+        new Wait(stack, 'wait some', {
+          time: WaitTime.duration(Duration.seconds(100)),
+        }).next(new AttiniMerge(stack, 'merges'))
+            .next(new AttiniMerge(stack, 'merge2')),
+  };
+  new DeploymentPlan(stack, 'testing-stack', props);
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('Attini::Deploy::DeploymentPlan', {});
+});
+
+
+test('should create template from list', () => {
+  const mockApp = new App();
+  const stack = new Stack(mockApp);
+  let props = {
+    states: [
+      new Wait(stack, 'wait some', {
+        time: WaitTime.duration(Duration.seconds(100)),
+      }),
+    ],
+  };
+  new DeploymentPlan(stack, 'testing-stack', props);
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('Attini::Deploy::DeploymentPlan', {});
+});
