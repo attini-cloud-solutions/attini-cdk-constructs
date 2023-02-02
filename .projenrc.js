27aba8dbcb22f31f233e22eaa9b821bea9cc7086
Carl <carl.ostrand@gmail.com>
2023-02-02 14:50:03 +0100

    new push test

--- .projenrc.js
+++ .projenrc.js
@@ -28,7 +28,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   publishToGo: {
     packageName: 'attini_cdk_lib',
-    moduleName: 'github.com/carlostrand/test'
+    moduleName: 'github.com/carlostrand/test',
     //'github.com/attini-cloud-solutions/attini-cdk-go/attinicdk',
     githubRepo: 'github.com/carlostrand/test.git',
     gitUserEmail: 'carl.ostrand@gmail.com',
