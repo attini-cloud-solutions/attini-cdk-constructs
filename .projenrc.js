bcd3aea15cabf4855e3dff6e654ba481d0e0f153
Carl <carl.ostrand@gmail.com>
2023-02-03 16:03:27 +0100

    new push test

--- .projenrc.js
+++ .projenrc.js
@@ -20,30 +20,29 @@ const project = new awscdk.AwsCdkConstructLibrary({
   publishToPypi: {
     distName: 'attini-cdk-lib',
     module: 'attini_cdk',
-    // twineRegistryUrl: 'https://upload.pypi.org/legacy/',
     twinePasswordSecret: 'TWINE_PASSWORD',
     twineUsernameSecret: 'TWINE_USERNAME',
   },
-  releaseToNpm: false,
+  releaseToNpm: true,
   npmTokenSecret: 'NPM_TOKEN',
 
   // publishToMaven: {
-  //   mavenGroupId: 'attini.cdk',
+  //   mavenGroupId: 'io.attini.cdk',
   //   mavenArtifactId: 'attini-cdk-lib',
   //   javaPackage: 'attini.cdk',
-  // },
-  // publishToGo: {
-  //   packageName: 'attini_cdk_lib',
-  //   moduleName: 'github.com/attini-cloud-solutions/attini-cdk-go',
-  //   githubRepo: 'github.com/attini-cloud-solutions/attini-cdk-go.git',
 
-  //   githubTokenSecret: 'GO_GITHUB_TOKEN',
-
-  //   //'github.com/attini-cloud-solutions/attini-cdk-go/attinicdk',
-  //   // gitUserEmail: 'carl.ostrand@gmail.com',
-  //   // gitUserName: 'carlostrand',
-  //   // githubUseSsh: false,
   // },
+  publishToGo: {
+    packageName: 'attini_cdk_lib',
+    moduleName: 'github.com/attini-cloud-solutions/attini-cdk-go',
+    githubRepo: 'github.com/attini-cloud-solutions/attini-cdk-go.git',
+    githubTokenSecret: 'GO_GITHUB_TOKEN',
+
+    //'github.com/attini-cloud-solutions/attini-cdk-go/attinicdk',
+    // gitUserEmail: 'carl.ostrand@gmail.com',
+    // gitUserName: 'carlostrand',
+    // githubUseSsh: false,
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Attini CDK
 
 The Attini CDK is currently an experimental feature.
+
+## How to use
+
+The Attini CDK Constructs are used to generate a CloudFormation template for
+(attini deplyment plans)[https://attini.io/attini-concepts/].
