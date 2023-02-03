a89d260fda8d81bf449e8487bff189bfc528b701
Carl <carl.ostrand@gmail.com>
2023-02-03 13:23:40 +0100

    new push test

--- .projenrc.js
+++ .projenrc.js
@@ -20,7 +20,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   publishToPypi: {
     distName: 'attini-cdk-lib',
     module: 'attini_cdk',
-    twineRegistryUrl: 'https://pypi.org/project/attini-cdk-lib/',
+    twineRegistryUrl: 'https://upload.pypi.org/',
   },
   releaseToNpm: true,
   npmTokenSecret: 'NPM_TOKEN',
