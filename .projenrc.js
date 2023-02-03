b6717c6bee1f638b7cf3f19f80276dc593720d12
Carl <carl.ostrand@gmail.com>
2023-02-03 10:22:41 +0100

    new push test

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
 
   bugsEmail: 'support@attini.io',
   author: 'oscarostrand',
-  description: 'Attini resources',
+  description: 'Attini CDK Constructs',
   copyrightOwner: 'Attini Cloud Solutions International AB',
 
   authorAddress: 'contact@attini.io',
@@ -22,23 +22,24 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // },
   releaseToNpm: true,
   npmTokenSecret: 'NPM_TOKEN',
-  publishToMaven: {
-    mavenGroupId: 'attini.cdk',
-    mavenArtifactId: 'attini-cdk-lib',
-    javaPackage: 'attini.cdk',
-  },
-  publishToGo: {
-    packageName: 'attini_cdk_lib',
-    moduleName: 'github.com/attini-cloud-solutions/attini-cdk-go',
-    githubRepo: 'github.com/attini-cloud-solutions/attini-cdk-go.git',
+  npm
+  // publishToMaven: {
+  //   mavenGroupId: 'attini.cdk',
+  //   mavenArtifactId: 'attini-cdk-lib',
+  //   javaPackage: 'attini.cdk',
+  // },
+  // publishToGo: {
+  //   packageName: 'attini_cdk_lib',
+  //   moduleName: 'github.com/attini-cloud-solutions/attini-cdk-go',
+  //   githubRepo: 'github.com/attini-cloud-solutions/attini-cdk-go.git',
 
-    githubTokenSecret: 'GO_GITHUB_TOKEN',
+  //   githubTokenSecret: 'GO_GITHUB_TOKEN',
 
-    //'github.com/attini-cloud-solutions/attini-cdk-go/attinicdk',
-    // gitUserEmail: 'carl.ostrand@gmail.com',
-    // gitUserName: 'carlostrand',
-    // githubUseSsh: false,
-  },
+  //   //'github.com/attini-cloud-solutions/attini-cdk-go/attinicdk',
+  //   // gitUserEmail: 'carl.ostrand@gmail.com',
+  //   // gitUserName: 'carlostrand',
+  //   // githubUseSsh: false,
+  // },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
