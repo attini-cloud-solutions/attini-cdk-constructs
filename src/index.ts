5935143686d8406d55b09782fdede95829c9e1b4
oscarostrand <oscar.ostrand@gmail.com>
2023-02-06 09:03:32 +0100

    props for DeploymentPlanStack is now optional

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export interface DeploymentPlanProps {
 
 export class DeploymentPlanStack extends Stack {
 
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     this.addTransform('AttiniDeploymentPlan');
     this.addTransform('AWS::Serverless-2016-10-31');
